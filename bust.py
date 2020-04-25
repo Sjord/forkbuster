cd2791f1fa09d15a414922748b020c4ce5202685
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2020-04-25 15:06:34 +0200

    Use test classes, print smaller exceptions

--- bust.py
+++ bust.py
@@ -2,40 +2,82 @@ import httpx
 import asyncio
 import sys
 
-test_files = set()
 
-def filetest(fname):
-    def decorator(func):
-        test_files.add((fname, func))
-        return func
-    return decorator
+class FileTest:
+    def __init__(self, origin):
+        self.origin = origin
+        self.url = self.origin + self.path
 
-@filetest(".git/HEAD")
-def open_git(response):
-    return response.content.startswith(b"ref: ")
+    def get_result(self, response):
+        if self.check_response(response):
+            return (self.url, type(self).__name__)
+        return None
 
-@filetest(".hg/requires")
-def open_hg(response):
-    return b"generaldelta" in response.content
 
-@filetest(".well-known/")
-def wellknown_index(response):
-    return b"Index of /.well-known/" in response.content
+class OpenGit(FileTest):
+    path = ".git/HEAD"
 
-async def search_origin(origin):
-    assert origin.endswith("/")
-    assert origin.startswith("http")
+    def check_response(self, response):
+        return response.content.startswith(b"ref: ")
 
-    # with httpx.Client() as client:
-    async with httpx.AsyncClient(http2=True, verify=False) as client:
-        for fname, found_func in test_files:
-            url = origin + fname
-            if found_func(await client.get(url)):
-                print(url)
-    
+
+class OpenMercural(FileTest):
+    path = ".hg/requires"
+
+    def check_response(self, response):
+        return b"generaldelta" in response.content
+
+
+class WellKnownIndex(FileTest):
+    path = ".well-known/"
+
+    def check_response(self, response):
+        return b"Index of /.well-known/" in response.content
+
+
+class OriginScan:
+    def __init__(self, origin):
+        assert origin.endswith("/")
+        assert origin.startswith("http")
+        self.origin = origin
+        self.tests = [t(origin) for t in FileTest.__subclasses__()]
+
+    async def scan(self):
+        # with httpx.Client(verify=False) as client:
+        async with httpx.AsyncClient(http2=True, verify=False) as client:
+            return [test.get_result(await client.get(test.url)) for test in self.tests]
+
+
+def show_status(todo, total):
+    percentage = 100 * (total - todo) / total
+    print("%.1f" % percentage, "%", end="\r")
+
+
+def print_results(task):
+    exc = task.exception()
+    if exc is not None:
+        print("X", str(exc) or type(exc).__name__)
+        return
+
+    results = filter(None, task.result())
+    for r in results:
+        print("!", r)
+
 
 async def bust(queue):
-    found = await asyncio.gather(*[search_origin(o.strip()) for o in queue], return_exceptions=True)
+    tasks = [OriginScan(o.strip()).scan() for o in queue]
+    total = len(tasks)
+    done = []
+
+    while tasks:
+        show_status(len(tasks), total)
+        done, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
+        for task in done:
+            print_results(task)
+
+    show_status(len(tasks), total)
+
 
 with open(sys.argv[1]) as fd:
     asyncio.get_event_loop().run_until_complete(bust(fd))
